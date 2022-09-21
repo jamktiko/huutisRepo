5fcb1798aed4ec2c13163a163fd5894307bce21d
Timo Saarela <N4065@student.jamk.fi>
2022-09-21 13:45:35 +0300

    koodin kommentointi ja huoneen tekemiseen frontend

--- huutoapp/src/app/app.module.ts
+++ huutoapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EtusivuComponent } from './etusivu/etusivu.component';
@@ -23,6 +24,7 @@ import { UsernameComponent } from './username/username.component';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- huutoapp/src/app/username/username.component.ts
+++ huutoapp/src/app/username/username.component.ts
@@ -13,8 +13,13 @@ export class UsernameComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  //name muuttujaan two way bindattu käyttjän syöttämä nimi, sekä mahdollinen
+  //satunnainen nimi, jos käyttäjä sellaisen valitsee
   name = '';
 
+
+  //funktio, joka valitsee satunnaisen nimen mikäli käyttäjä painaa nappia
   rndm(){
     let idx = Math.floor(Math.random() * (2738 - 1) + 1)
     this.name = names[idx]
