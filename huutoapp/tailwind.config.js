3048e81537934108261157e81ebb7c98c7eb39f2
Severi Natunen <AA4352@student.jamk.fi>
2022-10-05 14:42:29 +0300

    tailwindcss implementation

---
+++ huutoapp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        appblue: "#1fe5b6",
+      },
+    },
+  },
+  plugins: [],
+};
