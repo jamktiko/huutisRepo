1f6c6ff8b08ff9c698963d98123274454ca1988c
Jyri Lahtinen <n9516@student.jamk.fi>
2022-10-27 10:52:14 +0300

    CSS and HTML changes

--- huutoapp/tailwind.config.js
+++ huutoapp/tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      // TODO: Check and change these screen sizes to 
+      // approriate, these are default templates. 
+      // Also not 100% sure if we need to use these yet. --Jyri
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       colors: {
         appblue: "#1fe5b6",
