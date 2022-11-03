de674dd03880800bc2344a4291e2aff0455224b1
Severi Natunen <AA4352@student.jamk.fi>
2022-11-03 12:39:47 +0200

    PWA requirements

--- huutoapp/src/app/app.module.ts
+++ huutoapp/src/app/app.module.ts
@@ -11,6 +11,11 @@ import { VoteComponent } from './vote/vote.component';
 import { UsernameComponent } from './username/username.component';
 import { ResultComponent } from './result/result.component';
 import { WebsocketService } from './websocket.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+// Dont delete, might be crucial for the PWA to work, commented off for production -S
+// import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,8 +32,19 @@ import { WebsocketService } from './websocket.service';
     RouterModule,
     FormsModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    BrowserAnimationsModule,
   ],
-  providers: [WebsocketService],
+  providers: [
+    WebsocketService,
+    // Dont delete, might be crucial for the PWA to work, commented off for production -S
+    // { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
