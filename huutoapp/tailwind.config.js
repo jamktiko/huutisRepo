90b482a43288a69d8972ee3753b07d4f246eb10d
Jyri Lahtinen <n9516@student.jamk.fi>
2022-11-14 12:59:19 +0200

    Animated CSS background working

--- huutoapp/tailwind.config.js
+++ huutoapp/tailwind.config.js
@@ -16,11 +16,23 @@ module.exports = {
       // => @media (min-width: 1280px) { ... }
     },
     extend: {
-      // main colors to use in app
+      // main colors to use in app. You can find the whole style 
+      // guide in the Figma. 
+      // There are several hues of our main colors for use + black, 
+      // regular, dark and gray. -- Jyri
       colors: {
         appblue: "#1fe5b6",
+        /*appblue2: "#19b38f",
+          appblue3: "#0e6650",*/
+        appdarkblue: "#159979",
+        applightblue: "#d2faf0",
         appyellow: "#eefc5e",
+        appdarkyellow: "#c6d24e",
+        applightyellow: "#f6fdae",
         appblack: "#000000",
+        appgray: "#808080",
+        appdarkgray: "#555555",
+        applightgray: "#cccccc",
       },
     },
     // customed font stack -S
