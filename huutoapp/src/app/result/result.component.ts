58eb07550d766a6a944ea003f4402fe30ca82c7b
Severi Natunen <AA4352@student.jamk.fi>
2022-11-17 13:06:59 +0200

    Result componen more results view

--- huutoapp/src/app/result/result.component.ts
+++ huutoapp/src/app/result/result.component.ts
@@ -1,27 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'node_modules/chart.js';
 import { registerables } from 'chart.js';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css'],
+  animations: [
+    // trigger binded to the svg element -S
+    trigger('openClose', [
+      // if the state if open, rotate the element 90 degrees -S
+      state('open', style({ transform: 'rotate(90deg)' })),
+      transition('closed => open', [animate('0.01s')]),
+      transition('open => closed', [animate('0.01s')]),
+    ]),
+  ],
 })
 export class ResultComponent implements OnInit {
+  // boolean which changes based on if the "more results" is clicked in the ui -S
+  toDisplay = false;
+
+  // function that changes boolean above -S
+  toggleDisplay() {
+    this.toDisplay = !this.toDisplay;
+  }
+
+  // same boolean and function than above just for the animation on the arrow icon -S
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
   constructor() {}
 
   namearr = [
     {
-      vaihtoehto: 'pizza',
-      names: ['Aaran', 'Aaren', 'Aarez', 'Aarman', 'Aaron'],
+      vaihtoehto: 'Pizza',
+      names: ['Aaran', 'Aaren'],
     },
     {
-      vaihtoehto: 'pasta',
-      names: ['Aaran', 'Aaren', 'Aarez', 'Aarman', 'Aaron'],
+      vaihtoehto: 'Pasta',
+      names: ['Eero', 'Eemeli', 'Erkki'],
     },
     {
-      vaihtoehto: 'burgir',
-      names: ['Aaran', 'Aaren', 'Aarez', 'Aarman', 'Aaron'],
+      vaihtoehto: 'Burgir',
+      names: ['Vellamo', 'Veikko', 'Viljo', 'Vilho'],
     },
   ];
 
@@ -32,12 +62,12 @@ export class ResultComponent implements OnInit {
       type: 'bar',
       //labels for data, in real version these would be the voting options -S
       data: {
-        labels: ['pizza', 'pasta', 'burgir'],
+        labels: ['Pizza', 'Pasta', 'Burgir'],
         datasets: [
           {
             label: '# of Votes',
             //data and their representing colors, in real version these would be the results of the vote -S
-            data: [5, 5, 5],
+            data: [2, 3, 4],
             backgroundColor: [
               'rgba(27, 223, 6)',
               'rgba(31, 229, 182)',
