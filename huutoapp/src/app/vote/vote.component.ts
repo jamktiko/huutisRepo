d444e5bbfc68d2e3e6c58009f27f78f66367d837
Timo Saarela <N4065@student.jamk.fi>
2022-12-08 13:54:26 +0200

    Merge of timo and severinBranch

--- huutoapp/src/app/vote/vote.component.ts
+++ huutoapp/src/app/vote/vote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsockethandlerService } from '../AWSapi.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-vote',
@@ -6,44 +8,116 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vote.component.css'],
 })
 export class VoteComponent implements OnInit {
-  constructor() {}
+  id!: number;
 
-  ngOnInit(): void {}
+  status!: any;
 
-  ngOnDestroy() {}
+  messageFromServer!: any;
+  wsSubscription!: Subscription;
 
-  idx = '';
+  currrentNameSubscr!: Subscription;
+  currentName!: any;
 
-  public vastausvaihtoehdot = [
-    {
-      vastausvaihtoehto: '1',
-    },
-    {
-      vastausvaihtoehto: '2',
-    },
-    {
-      vastausvaihtoehto: '3',
-    },
-    {
-      vastausvaihtoehto: '4',
-    },
-    {
-      vastausvaihtoehto: '5',
-    },
-    {
-      vastausvaihtoehto: '6',
-    },
-  ];
+  voteLimit!: number;
+  votes: number = 0;
 
-  // function that selects random choice to vote when user presses the "choose for me" button -S
-  random() {
-    this.vastausvaihtoehdot[
-      Math.floor(Math.random() * this.vastausvaihtoehdot.length)
-    ];
+  constructor(private AWS: WebsockethandlerService) {}
+
+  ngOnInit(): void {
+    this.AWS.bindFunction(this.getRoomData.bind(this));
+
+    console.log(this.AWS.wsSubscription);
+    if (!this.AWS.messageFromServer) {
+      this.AWS.messageFromServer = JSON.parse(
+        sessionStorage.getItem('roomData') || '{}'
+      );
+    }
+
+    if (this.AWS.wsSubscription === undefined) {
+      this.AWS.initSocket();
+      this.AWS.hasReconnected = true;
+    }
+
+    this.messageFromServer = this.AWS.messageFromServer;
+
+    this.voteLimit = this.AWS.messageFromServer.Item.votelimit;
+    this.currrentNameSubscr = this.AWS.currentName.subscribe(
+      (name) => (this.currentName = name)
+    );
   }
 
-  vote(data: any) {
-    this.idx = data;
-    console.log(this.idx);
+  disableButton(event: any) {
+    event.target.disabled = true;
+  }
+
+  sendVote(index: any, id: any) {
+    if (sessionStorage.getItem('hasVoted') !== '1') {
+      this.votes += 1;
+      console.log(this.votes);
+      const msg: {
+        roomId: string;
+        action: string;
+        choice: string;
+        name: string;
+      } = {
+        action: 'incrementChoiceVotes',
+        roomId: id,
+        choice: JSON.stringify(index),
+        name: this.currentName,
+      };
+      this.status = this.AWS.sendMessage(JSON.stringify(msg));
+      console.log(JSON.stringify(msg));
+      if (this.AWS.messageFromServer.Item.votelimit == this.votes) {
+        if (this.AWS.hasReconnected) {
+          this.AWS.saveConnection(
+            JSON.parse(sessionStorage.getItem('roomData') || '{}').Item.roomId
+          );
+        }
+      }
+    }
+  }
+
+  checkReconnect() {
+    if (this.AWS.hasReconnected) {
+      this.AWS.saveConnection(
+        JSON.parse(sessionStorage.getItem('roomData') || '{}').Item.roomId
+      );
+    }
+  }
+
+  random() {
+    let arr: any = [];
+    for (
+      let i = this.votes;
+      i < this.AWS.messageFromServer.Item.votelimit;
+      i++
+    ) {
+      let idx = Math.floor(
+        Math.random() * this.AWS.messageFromServer.Item.choices.length
+      );
+
+      while (arr.includes(idx)) {
+        idx = Math.floor(
+          Math.random() * this.AWS.messageFromServer.Item.choices.length
+        );
+      }
+
+      if (!arr.includes(idx)) {
+        this.sendVote(idx, this.AWS.messageFromServer.Item.roomId);
+        arr.push(idx);
+        console.log(arr);
+      }
+    }
+  }
+
+  //this function is called when the socket receives a message and checks if
+  //the current data in the messageFromServer data is valid and replaces it if it is not valid.
+  getRoomData() {
+    if (
+      this.messageFromServer.Item === undefined ||
+      this.messageFromServer.Item.roomId != sessionStorage.getItem('roomId')
+    ) {
+      this.ngOnInit();
+    }
   }
 }
